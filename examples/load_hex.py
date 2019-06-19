5571eb17bac617cdb7598f5dff6f9735e01b83ab
Greg Steiert <greg.steiert@latticesemi.com>
2019-06-19 16:33:50 -0700

    moved load_hex function into machxo_i2c library, removed machxo_hex library

--- examples/load_hex.py
+++ examples/load_hex.py
@@ -1,7 +1,7 @@
 import board
 import busio
-import machxo_hex
-from machxo_hex import MachXOi2c 
+import machxo_i2c
+from machxo_i2c import MachXOi2c 
 i2c = busio.I2C(board.SCL, board.SDA)
 machxo = MachXOi2c()
 machxo.init(i2c)
--- machxo_i2c.py
+++ machxo_i2c.py
@@ -225,3 +225,40 @@ class MachXOi2c:
                         print("bad page data format")
             line = jed_file.readline().rstrip()
         jed_file.close()
+
+    def load_hex(self, filename):
+        hex_file = open(filename, 'r')
+        count = 0
+        self.reset_config_addr()
+        line = hex_file.readline().rstrip()
+        while line:
+            if len(line) == 32:
+                page_data = [ 
+                    int(line[0:2], 16),
+                    int(line[2:4], 16),
+                    int(line[4:6], 16),
+                    int(line[6:8], 16),
+                    int(line[8:10], 16),
+                    int(line[10:12], 16),
+                    int(line[12:14], 16),
+                    int(line[14:16], 16),
+                    int(line[16:18], 16),
+                    int(line[18:20], 16),
+                    int(line[20:22], 16),
+                    int(line[22:24], 16),
+                    int(line[24:26], 16),
+                    int(line[26:28], 16),
+                    int(line[28:30], 16),
+                    int(line[30:32], 16),
+                    ]
+                self.program_page(page_data)                       
+# It takes >400us to load command +128 bits at 400kHz
+# You shouldn't need to check the busy flag with I2C
+#                self.wait_busy()
+                count += 1
+            else:
+                print("expected 32 bytes, got ", len(line), " at line ", count) 
+            line = hex_file.readline().rstrip()
+        print("wrote ", count, " pages")
+        hex_file.close()
+        self.wait_busy()
